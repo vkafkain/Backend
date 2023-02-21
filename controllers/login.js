c6b097e4e1ff32ac3d7726163fbb974328d573b6
reinevernunft13 <96286318+reinevernunft13@users.noreply.github.com>
2023-02-21 13:00:40 +0100

    update

---
+++ controllers/login.js
@@ -0,0 +1,51 @@
+const User = require('../models/User.js');
+const { invalidInput } = require('../controllers/errorHandler')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const login = async (req, res) => {
+
+    const userName = req.body.userName;
+    const password = req.body.password;
+    
+    //console.log('user input is :' + userName, password);
+
+    if(!userName || !password) {
+        return invalidInput(req, res);
+    }
+   
+    const userFound = await User.findOne({ userName });
+
+    if(!userFound) {
+        return res.status(401).json({
+            status: 'error',
+            error: '401 - Unauthorized Access: Invalid username/password'
+        })
+    }
+
+    const validPassword = await bcrypt.compare(password, userFound.password);
+
+    if(validPassword) {
+            
+        const payload = {
+            userId: userFound._id,
+            userName: userFound.userName
+        }
+        const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_KEY);
+        
+        return res.status(200).json({
+            status: 'ok',
+            payload: payload,
+            accessToken: accessToken
+        });
+    
+    } else {
+        return res.status(401).json({
+            status: 'error',
+            error: '401 - Unauthorized Access: Invalid username/password'
+        })
+    }
+ 
+}
+
+module.exports = login;
