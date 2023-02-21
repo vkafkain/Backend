e0c0560e66ab248e82f80e169a61df9134ea4d80
vkafkain <vlopezmoles@gmail.com>
2023-02-21 12:17:14 +0100

    init index

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require("express");
+const cors = require("cors")
+const { pageNotFound } = require("./controllers/errorHandler");
 const app = express();
+const connectDB = require("./database/db.connection")
+const PORT = process.env.SERVER_PORT || 3000;
+
+connectDB();
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+
+app.use('/', require(routes));
+
+app.use(pageNotFound);
+
+app.listen(
+    PORT,
+    console.log(`Server listening on port ${PORT}`)
+);
